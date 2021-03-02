d5c8961426274a7c305ad1c81175603cb02fad73
Abdo Rottab <arotab@effvision.com>
2021-03-02 14:08:11 +0200

    first

--- __manifest__.py
+++ __manifest__.py
@@ -6,9 +6,10 @@
     'author': 'EffVision',
     'category': 'Inventory',
 
-    'depends': ['product'],
+    'depends': ['product', 'stock', 'purchase', 'crm'],
     'data': [
         'security/ir.model.access.csv',
+        'data/product.xml',
         'views/product_view.xml',
     ],
 
--- models/product.py
+++ models/product.py
@@ -8,17 +8,36 @@ class MountingType(models.Model):
     name = fields.Char(string='Name')
 
 
+class ProductFamilyName(models.Model):
+    _name = 'product.family.name'
+
+    name = fields.Char(string='Name')
+
+
+class ProductColor(models.Model):
+    _name = 'product.color'
+
+    name = fields.Char(string='Name')
+
+
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    family_name = fields.Char(string='Family Name')
-    color = fields.Char(string='Color')
+    family_name = fields.Many2one(comodel_name='product.family.name',
+                                  string='Family Name')
+    color = fields.Many2one(comodel_name='product.color',
+                            string='Color', help='Color Corrected Temperature')
+    mounting_type = fields.Many2one(comodel_name='product.mounting.type',
+                                    string='Mounting Type')
     power = fields.Char(string='Power')
     lumen = fields.Char(string='Lumen')
     product_code = fields.Char(string='Product Code')
+    product_type = fields.Char(string='Product Type')
     cct = fields.Char(string='CCT')
     ip = fields.Char(string='IP Rating')
     led_voltage = fields.Char(string='LED Voltage')
+    driver_manufacture = fields.Many2one(comodel_name='res.partner', string='Driver Manufacturer',
+                                         domain="[('is_company', '=', True)]")
 
 
 class Product(models.Model):
