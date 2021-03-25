caaacb1761224af63b0f9e4ac3130d6b0bba40e2
Rottab <mr.rottab@gmail.com>
2021-03-25 19:01:13 +0200

    cron

--- egy-trade_custom/__manifest__.py
+++ egy-trade_custom/__manifest__.py
@@ -12,12 +12,13 @@
         'data/one_time.xml',
         'data/product.xml',
         'data/stock_picking.xml',
+        'wizard/change_stage_wizard.xml',
         'views/res_partner_view.xml',
         'views/stock_view.xml',
         'views/stock_picking_view.xml',
         # 'views/purchase_view.xml',
         'views/product_view.xml',
-        'wizard/change_stage_wizard.xml',
+
     ],
     'application': True,
     'external_dependencies': {'python': ['pandas']}
