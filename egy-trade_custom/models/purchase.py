a799ddfcaa460636d36b19982efe98676a23f486
Rottab <mr.rottab@gmail.com>
2021-09-03 18:20:16 +0200

    printable sale/purchase orders edits

--- egy-trade_custom/models/purchase.py
+++ egy-trade_custom/models/purchase.py
@@ -9,3 +9,13 @@ class PurchaseOrderLine(models.Model):
     @api.onchange('partner_id')
     def onchange_partner_id(self):
         self.order_line = False
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = 'purchase.order.line'
+
+    product_vendor = fields.Many2one(related='product_id.vendor_id')
+    product_family_name = fields.Many2one(related='product_id.family_name')
+    product_power = fields.Char(related='product_id.power')
+    product_ip = fields.Char(related='product_id.ip')
+    product_lumen = fields.Char(related='product_id.lumen')
