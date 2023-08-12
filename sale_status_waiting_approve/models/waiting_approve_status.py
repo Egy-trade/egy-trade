0b020c86af93c75d8f13fb89a3e4d38188313378
elfekyitss <mahmoud.elfeky@itss-c.com>
2023-08-13 02:25:04 +0300

    merge 16 addons

--- sale_status_waiting_approve/models/waiting_approve_status.py
+++ sale_status_waiting_approve/models/waiting_approve_status.py
@@ -6,7 +6,7 @@ from odoo import models, fields, api, _
 class inherit_sale(models.Model):
     _inherit = "sale.order"
 
-    state = fields.Selection(selection_add=[('waiting_approve', 'Waiting Approve')])
+    # state = fields.Selection(selection_add=[('waiting_approve', 'Waiting Approve')])
 
     def action_approve(self):
         self.write({'state': 'draft'})
